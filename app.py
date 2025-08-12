481ad78e5c17579ebb43d540b8126f467a4a5df0
SunnyPanchani <sunnypanchani9007@gmail.com>
2025-08-12 17:22:23 +0530

    fy11

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from ultralytics import YOLO
+from PIL import Image
+
+# Load model
+model = YOLO("best.pt")
+
+st.title("YOLO Classification App")
+uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    if st.button("Predict"):
+        results = model(image)
+        probs = results[0].probs
+        class_id = probs.top1
+        class_name = model.names[class_id]
+        st.success(f"Prediction: {class_name} ({probs.top1conf:.2f} confidence)")
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'fire-smoke-detec',
+    version= '0.0.0',
+    author= 'Sunny Panchani',
+    author_email= 'sunnypanchani9099@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
+
+
+
+
+# https://www.youtube.com/watch?v=cr17R0fyVXc
